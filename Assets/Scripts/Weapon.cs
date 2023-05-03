7691f504bd26a470e60ab8e4ca0654fa7814f43e
Aedan-Vongsouthi-Student <112557510+Aedan-Vongsouthi-Student@users.noreply.github.com>
2023-05-03 10:00:37 -0400

    Got gun

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,24 +8,45 @@ public class Weapon : MonoBehaviour
     Player player;
     [SerializeField] GameObject bullet;
     [SerializeField] Transform gun;
-    public int amountOfBullets;
-    public float spread, bulletspeed;
+    [SerializeField] public float offset;
+    [SerializeField] public int amountOfBullets;
+    [SerializeField] public float spread, bulletspeed;
     
     void Awake() 
     {
-        player = GetComponent<Player>();    
+        Transform parent = gameObject.transform.parent;
+        player = parent.gameObject.GetComponent<Player>();
+    }
+
+    private void Update() 
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle + offset, Vector3.forward);
     }
 
     void OnFire(InputValue value)
     {
         if(player.isAlive)
         {
-            Instantiate(bullet, gun.position, transform.rotation);
+            Shoot();
         } else {
             return;
         }
     }
-    
+
+    void Shoot()
+    {
+        for (int i = 0; i < amountOfBullets; i++)
+            {
+               GameObject b = Instantiate(bullet, gun.position, transform.rotation);
+               Rigidbody2D brb = b.GetComponent<Rigidbody2D>();
+               Vector2 dir = transform.rotation * Vector2.up;
+               Vector2 pdir = Vector2.Perpendicular(dir) * Random.Range(-spread, spread);
+               brb.velocity = (dir * pdir) * bulletspeed;
+            } 
+    }
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Enemy")
