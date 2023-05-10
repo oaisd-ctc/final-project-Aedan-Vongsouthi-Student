4bd3c5c0fdf52ed5e954af119f117ba37c644633
AedanVongsouthi <aevo910@student.oaisd.org>
2023-05-10 07:58:08 -0400

    Fade In and Fade Out

---
+++ Assets/Scripts/TextFade.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextFade : MonoBehaviour
+{
+    [SerializeField] TMP_Text textmeshPro;
+    byte alpha;
+    public void Start()
+    {
+        textmeshPro = GetComponent<TMP_Text>();
+        alpha = 0;
+        textmeshPro.color = new Color32(255,255,255, alpha);
+    }
+
+    
+    void Update()
+    {
+
+        if (alpha <= 0)
+        {
+            StopCoroutine("FadeOut");
+        } else if (alpha >= 255)
+        {
+            StopCoroutine("FadeIn");
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        for (alpha = 255; alpha >= 0; alpha--)
+        {
+            textmeshPro.color = new Color32(255,255,255, alpha);
+            yield return null;
+        }
+
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (alpha = 0; alpha <= 255; alpha++)
+        {
+            textmeshPro.color = new Color32(255,255,255, alpha);
+            yield return null;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine("FadeIn");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine("FadeOut");
+        }
+    }
+}
